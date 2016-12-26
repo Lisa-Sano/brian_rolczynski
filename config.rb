83e954d235522a0809238ce592f7bd496ff4fccc
Lisa Rolczynski <lisa.sano@gmail.com>
2016-12-26 15:30:40 -0800

    Add pages for the sections and create separate layout. Highlight current page in nav. Widen Jablonski diagram.

--- config.rb
+++ config.rb
@@ -2,15 +2,21 @@
 # Page options, layouts, aliases and proxies
 ###
 
+activate :directory_indexes
+
 # Per-page layout changes:
 #
 # With no layout
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+page "/sitemap.xml", layout: false
 
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
+page "/publications/*", layout: :section
+page "/research/*", layout: :section
+page "/curriculum_vitae/*", layout: :section
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
@@ -27,6 +33,19 @@ end
 # Helpers
 ###
 
+
+def page_classes
+  path = request.path_info.dup
+  path << settings.index_file if path.match(%r{/$})
+  path = path.gsub(%r{^/}, '')
+
+  classes = []
+  parts = path.split('.')[0].split('/')
+  parts.each_with_index { |path, i| classes << parts.first(i+1).join('_') }
+
+  classes.join(' ')
+end
+
 # Methods defined in the helpers block are available in templates
 # helpers do
 #   def some_helper
@@ -37,8 +56,10 @@ end
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
+
+  activate :relative_assets
 end
