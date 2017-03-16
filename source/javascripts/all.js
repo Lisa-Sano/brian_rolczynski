94442fd5b8567b9f5c44dcacefc7cca04a808149
Lisa Rolczynski <lisa.sano@gmail.com>
2017-03-16 16:52:28 -0700

    add publications and images for publications

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
   $('map').imageMapResize();
+
+  $('.toggle-link').click(function() {
+    $(this).find("span").toggleClass("hidden");
+    $(this).parent().find("br").toggleClass("hidden");
+  });
+  
+  $('#expand-all').click(function() {
+    $('.publications-section .collapse').collapse('show');
+    $('.publications-section .expand').addClass('hidden');
+    $('.publications-section .retract').removeClass('hidden');
+    $('.publications-section br').removeClass('hidden');
+  });
+
+  $('#retract-all').click(function() {
+    $('.publications-section .collapse').collapse('hide');
+    $('.publications-section .expand').removeClass('hidden');
+    $('.publications-section .retract').addClass('hidden');
+    $('.publications-section br').addClass('hidden');
+  });
 });
