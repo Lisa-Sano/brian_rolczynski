73503662433e77026b616d477c579e98c5902c23
Lisa Rolczynski <lisa.sano@gmail.com>
2017-03-19 11:28:32 -0700

    add gifs that animate on hover of a navbar icon. add about me timeline to page

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -19,4 +19,28 @@ $(document).ready(function() {
     $('.publications-section .retract').addClass('hidden');
     $('.publications-section br').addClass('hidden');
   });
+
+  $('#research-icon').hover(function() {
+    $(this).attr('src','/gifs/RESEARCH.gif');
+  },function() {
+    $(this).attr('src','/images/RESEARCH_ICON.png');
+  });
+
+  $('#publications-icon').hover(function() {
+    $(this).attr('src','/gifs/PAPERS.gif');
+  },function() {
+    $(this).attr('src','/images/PUBLICATIONS_ICON.png');
+  });
+
+  $('#cv-icon').hover(function() {
+    $(this).attr('src','/gifs/CV.gif');
+  },function() {
+    $(this).attr('src','/images/CV_ICON.png');
+  });
+
+  $('#about-icon').hover(function() {
+    $(this).attr('src','/gifs/ABOUTME.gif');
+  },function() {
+    $(this).attr('src','/images/ABOUT_ICON.png');
+  });
 });
