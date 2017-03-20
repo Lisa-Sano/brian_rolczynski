e254bb062a32b2d34379f9585754ef3fcfa1b53a
Lisa Rolczynski <lisa.sano@gmail.com>
2017-03-19 21:35:37 -0700

    update how the expand and collapse all works

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,23 +1,22 @@
 $(document).ready(function() {
   $('map').imageMapResize();
 
-  $('.toggle-link').click(function() {
-    $(this).find("span").toggleClass("hidden");
-    $(this).parent().find("br").toggleClass("hidden");
-  });
-  
-  $('#expand-all').click(function() {
-    $('.publications-section .collapse').collapse('show');
-    $('.publications-section .expand').addClass('hidden');
-    $('.publications-section .retract').removeClass('hidden');
-    $('.publications-section br').removeClass('hidden');
+  $('.toggle-link').click(function(){
+    $(this).text(function(i,old){
+        return old=='[EXPAND]' ?  '[RETRACT]' : '[EXPAND]';
+    });
   });
 
-  $('#retract-all').click(function() {
-    $('.publications-section .collapse').collapse('hide');
-    $('.publications-section .expand').removeClass('hidden');
-    $('.publications-section .retract').addClass('hidden');
+  $('#pub-expand-all').click(function() {
+    $('.publications-section br').removeClass('hidden');
+    $('.publications-section .collapse').collapse('show');
+    $('.toggle-link').text('[RETRACT]');
+  });
+
+  $('#pub-retract-all').click(function() {
     $('.publications-section br').addClass('hidden');
+    $('.publications-section .collapse').collapse('hide');
+    $('.toggle-link').text('[EXPAND]');
   });
 
   $('#research-icon').hover(function() {
